f461dc4290bbf7fed2a6b5a36da9685c42b5e79e
dingyuc <dingyuc@yonyou.com>
2021-02-23 20:39:03 +0800

    2021-02-23 20:39

---
+++ src/assets/libs/languages/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import zh from './zh-CN.js';
+import tw from './zh-TW.js';
+import en from './en-US.js';
+let globalENVObj = require('../../utils/globalENVObj.js');
+
+Vue.use(VueI18n);
+
+let language = '';
+if(window.WEB_DIWORK_GLOBAL_CONFIG) {
+    language = window.WEB_DIWORK_GLOBAL_CONFIG.lang ? (window.WEB_DIWORK_GLOBAL_CONFIG.lang).toLocaleLowerCase().replace('_', '-') : 'zh-cn';
+}else {
+    language = (window.navigator.userLanguage || window.navigator.language).toLocaleLowerCase();
+}
+let i18nLocalLanguage = 'zh';
+let currentLocale = zh;
+// 英文:en-US  中文/中文简体:zh-CN   繁体:zh-TW
+if (language.indexOf('en') >= 0) {
+    language = 'en';
+    i18nLocalLanguage = 'en';
+    currentLocale = en;
+} else if (language == 'zh-tw') {
+    language = 'zh-tw';
+    i18nLocalLanguage = 'tw';
+    currentLocale = tw;
+} else {
+    language = 'zh-cn';
+    i18nLocalLanguage = 'zh';
+    currentLocale = zh;
+}
+globalENVObj.globalUniquenessCoordinationCliTopLevelObj = globalENVObj.globalUniquenessCoordinationCliTopLevelObj || {};
+globalENVObj.globalUniquenessCoordinationCliTopLevelObj.currentBrowserLanguage = i18nLocalLanguage;
+
+const i18n = new VueI18n({
+    locale: i18nLocalLanguage,
+    messages: {
+        zh,
+        tw,
+        en,
+    }
+});
+export default {
+    i18n,
+    locale: currentLocale
+};
--- src/index.vue
+++ src/index.vue
@@ -5,6 +5,8 @@
     <router-view></router-view>
 </template>
 <script lang="ts">
+import './assets/css/reset.css';
+import './assets/css/coordination-cli.less';
 import {defineComponent} from 'vue';
 
 export default defineComponent({
