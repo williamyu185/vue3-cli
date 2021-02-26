e19f357d220ce75a6afdc1f1c924222c4733c43d
dingyuc <dingyuc@yonyou.com>
2021-02-26 14:17:44 +0800

    2021-02-26 14:17

--- typings/shims-vue.d.ts
+++ typings/shims-vue.d.ts
@@ -1,11 +1,13 @@
-
-
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
   const component: DefineComponent<{}, {}, any>
   export default component;
 }
-
+// declare module 'vue/types/vue' {
+//   interface Vue {
+//     $http: any
+//   }
+// }
 // declare module 'Vue/types/vue' {
 //   import { AxiosInstance } from 'axios';
 //   interface Vue {
